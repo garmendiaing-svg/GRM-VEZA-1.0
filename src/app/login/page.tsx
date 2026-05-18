f6d73c4e1f8c690492e72e787869ad8f4c4bc1d5
garmendiaing-svg <garmendiaing@gmail.com>
2026-05-17 20:56:30 -0400

    Add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from "react";
+
+import { LoginForm } from "@/components/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center px-4 py-10">
+      <Suspense>
+        <LoginForm />
+      </Suspense>
+    </main>
+  );
+}
